49331ca3ba687b52b755b8df793054ec709a878c
Zahid-Hasan-Jony <zahidjony52@gmail.com>
2019-04-28 07:29:31 +0600

    Led control using button and arduino

---
+++ ledbutton.ino
@@ -0,0 +1,23 @@
+#define LED 11
+#define BUTTON 10
+
+void setup() {
+  pinMode(11, OUTPUT);
+  pinMode(10, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if (digitalRead(10) == HIGH) {
+    digitalWrite(LED, HIGH);
+    int  data=digitalRead(10);
+    Serial.println(data);
+  }
+  
+  else {
+    digitalWrite(LED, LOW);
+    int  data=digitalRead(10);
+    Serial.println(data);
+}
+}
