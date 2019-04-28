6d04151a5f14b288e59a7d9aa937b9788d72f171
Zahid-Hasan-Jony <zahidjony52@gmail.com>
2019-04-28 16:30:53 +0600

    Detect tank water level and start pump automatically

---
+++ motor_pump.ino
@@ -0,0 +1,38 @@
+int led=13;
+int trig=3;
+int echo=2;
+int motor=4;
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(motor, OUTPUT);
+  digitalWrite(motor,HIGH);
+
+}
+
+void loop() {
+    int duration , distance;
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH);
+  distance = (duration / 2) / 21.9;
+  Serial.print(distance);
+  Serial.println("cm");
+ delay(50);
+ if (distance>33) {
+  digitalWrite(motor,LOW);
+  digitalWrite(led,HIGH);
+  Serial.println("Motor ON");
+ }
+ else if(distance<6){
+  
+  digitalWrite(motor,HIGH);
+  digitalWrite(led,LOW);
+  Serial.println("Motor OFF");
+ }
+
+}
